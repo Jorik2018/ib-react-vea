52ac52ffa1c10f9927710d3ffecc07b4d26e4c90
vetatrem <vegatarazona.t@outlook.com>
2022-08-16 22:20:38 -0500

    Second commit

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<!doctype html><html lang="en"><head><meta charset="utf-8"/><link rel="icon" href="/favicon.ico"/>
+<meta name="viewport" content="width=device-width,initial-scale=1"/><meta name="theme-color" content="#000000"/><meta name="description" 
+content="Web site created using create-react-app"/><link rel="apple-touch-icon" href="/logo192.png"/><link rel="manifest" href="/manifest.json"/>
+<title>React App</title>
+<?php
+echo "hello";
+?>
+<script defer="defer" src="/build/static/js/main.127b8841.js"></script>
+<link href="/build/static/css/main.073c9b0a.css" rel="stylesheet">
+
+
+</head><body>
+    <noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div></body>
+    </html>
